609fec128325fd9d230e295abdc68eccc2831559
alexandre_ferreira <alexandre.vini.ferreira@gmail.com>
2021-05-28 22:40:23 -0300

    adding cython as dependency

--- setup.py
+++ setup.py
@@ -4,22 +4,13 @@ from setuptools.command.build_ext import build_ext as _build_ext
 import sys
 import warnings
 
-try:
-    from Cython.Build import cythonize
-    USE_CYTHON = True
-except ImportError:
-    USE_CYTHON = False
+from Cython.Build import cythonize
+USE_CYTHON = True
 
 # numpy path is needed for building with and without cython:
-try:
-    import numpy
-    numpy_includes = [numpy.get_include()]
-    HAVE_NUMPY = True
-except ImportError:
-    # "python setup.py build" will not work and trigger fallback to pure python later on,
-    # but "python setup.py clean" will be successful with the first call of setup(...)
-    numpy_includes = []
-    HAVE_NUMPY = False
+import numpy
+numpy_includes = [numpy.get_include()]
+HAVE_NUMPY = True
 
 classifiers = [
     'Programming Language :: Python :: 2',
@@ -39,7 +30,7 @@ class build_ext(_build_ext):
         self.include_dirs.append(numpy.get_include())
 
 
-ext = '.pyx' if USE_CYTHON else '.cpp'
+ext = '.pyx'
 
 extensions = [Extension(
         'fastdtw._fastdtw',
@@ -49,8 +40,7 @@ extensions = [Extension(
         libraries=["stdc++"]
     )]
 
-if USE_CYTHON:
-    extensions = cythonize(extensions)
+extensions = cythonize(extensions)    
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(dir_path, 'README.rst')) as f:
@@ -77,10 +67,4 @@ kwargs = {
     'classifiers': classifiers
 }
 
-try:
-    setup(**kwargs)
-except SystemExit:
-    del kwargs['ext_modules']
-    reason = 'numpy missing, ' if not HAVE_NUMPY else ''
-    warnings.warn(reason+'compilation failed. Installing pure python package')
-    setup(**kwargs)
+setup(**kwargs)
